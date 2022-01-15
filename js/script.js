a3a8f414a6e25763a949fc2113b8fa0e62d0b373
Cocotero98 <aguilarag98@gmail.com>
2022-01-15 14:39:29 -0300

    js added

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let daynames=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+]
+let months=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aougust",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+let d= new Date();
+let dayname= daynames[d.getDay()];
+let month= d.getMonth
+let date= d.getDate
+let year= d.getFullYear
+let hour= d.getHours
+let minute= d.getMinutes
+let seconds= d.getSeconds
+
+let fulldate=`${month}'/'${date}'/'${year}' '${hour}':'${minute}':'${seconds}`
