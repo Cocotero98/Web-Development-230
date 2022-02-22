bf0f48a3eca3c5fa66a45215bf1de767a22986dc
Cocotero98 <aguilarag98@gmail.com>
2022-02-22 15:16:13 -0300

    Wind Chill bug corrected

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -6,11 +6,27 @@ let windSpeed=document.getElementById('windSpeed').innerHTML
 wSpeedInt=parseFloat(windSpeed)
 temperatureInt=parseInt(temperature,10)
 
+let f=function(c){
+    return (c*1.8)+32
+}
+let farenheit=f(temperatureInt)
+
+let m=function (k){
+    return  k*0.6214
+}
+let miles= m(wSpeedInt)
+
+let c=function(f){
+    return (f-32)/1.8
+}
+
 let windChillCalculator= function(t,s){
-    let windCill=''
-    if (t<=10 && s>=4.8){
-        windCill=35.74+(0.6215*t)-35.75*(s**0.16)+(0.4275*t)*(s**0.16)
-        return windCill
+    let windChill=''
+    if (t<=50 && s>=3.0){
+        windChill=35.74+(0.6215*t)-35.75*(s**0.16)+(0.4275*t)*(s**0.16)
+        return windChill
     }else{return 'N/A'}
 }
-document.getElementById('windChill').innerHTML=windChillCalculator(temperatureInt,wSpeedInt).toFixed(2)
+let resultInFarenheit=windChillCalculator(farenheit,miles)
+let windChillInC=c(resultInFarenheit)
+document.getElementById('windChill').innerHTML=`Wind Chill: ${windChillInC.toFixed(1)}°C`
