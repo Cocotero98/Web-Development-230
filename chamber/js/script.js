0d57cc7992e481ee7c0c0c9853f9e0b2a4c48277
Cocotero98 <aguilarag98@gmail.com>
2022-02-23 13:32:11 -0300

    More changes to discover

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -2,6 +2,7 @@ document.querySelector(
 	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
 
+
 const now = new Date();
 
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
@@ -35,4 +36,32 @@ else if(day===2){
     toggleBanner()
 }
 
-// Wednesday, 24 July 2020.
+let imagesToLoad= document.querySelectorAll('source[data-src]');
+const changeAttribute= function(image){
+    image.setAttribute('srcset',image.getAttribute('data-src'))
+    image.onload=()=>{image.removeAttribute('data-src')}
+}
+
+const imgOptions={
+    threshold: 0,
+    rootMargin: '0px 0px 400px 0px'
+}
+
+window.onload=()=>{
+    if ('IntersectionObserver' in window){
+        const observer= new IntersectionObserver((items,observer)=>{
+            items.forEach((item)=>{
+                if (item.isIntersecting){
+                    changeAttribute(item.target);
+                    observer.unobserve(item.target);
+                }
+            })
+        },imgOptions);
+        imagesToLoad.forEach((img)=>{
+        observer.observe(img)})
+    }
+}
+
+
+let counter= document.getElementById('counter')
+counter.innerText='Hi'
