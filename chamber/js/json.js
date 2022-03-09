75087c5879056bbd9f21bf43c9f33c139229e379
Cocotero98 <aguilarag98@gmail.com>
2022-03-08 23:53:23 -0300

    .

--- chamber/js/json.js
+++ chamber/js/json.js
@@ -8,30 +8,30 @@ const doStuf = function(data){
     img.setAttribute('src', image);
     newElement.appendChild(img)
 }
-const displayBusiness= function(business){
-    let image=business.image;
-    let name=business.name;
-    let adress=business.adress;
-    let phone=business.phone;
-    let website=business.website;
-    let card = document.createElement('section');
-    let img=document.createElement('img');
-    img.setAttribute('src', image);
-    img.setAttribute('alt', name);
-    let p1=document.createElement('p');
-    let p2=document.createElement('p');
-    let p3=document.createElement('p')
-    p1.innerText=adress;
-    p2.innerText=phone;
-    p3.innerText=website;
-    card.appendChild(img);
-    card.appendChild(p1);
-    card.appendChild(p2);
-    card.appendChild(p3);
-    business.innerHTML=phone;
-}
+// const displayBusiness= function(business){
+//     let image=business.image;
+//     let name=business.name;
+//     let adress=business.adress;
+//     let phone=business.phone;
+//     let website=business.website;
+//     let card = document.createElement('section');
+//     let img=document.createElement('img');
+//     img.setAttribute('src', image);
+//     img.setAttribute('alt', name);
+//     let p1=document.createElement('p');
+//     let p2=document.createElement('p');
+//     let p3=document.createElement('p')
+//     p1.innerText=adress;
+//     p2.innerText=phone;
+//     p3.innerText=website;
+//     card.appendChild(img);
+//     card.appendChild(p1);
+//     card.appendChild(p2);
+//     card.appendChild(p3);
+//     business.innerHTML=phone;
+// }
 
 
 fetch('https://cocotero98.github.io/wdd230/chamber/data/data.json')
-.then((response)=>{if (response.ok){return response.json(), console.log('yes')}else{console.log('error')}})
+.then((response)=>{if (response.ok){console.log('yes')}else{console.log('error')}})
 .then(doStuf())
