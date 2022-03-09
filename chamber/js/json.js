05c15a3420f07c187881f424e47face1adb1f4e5
Cocotero98 <aguilarag98@gmail.com>
2022-03-08 23:01:12 -0300

    New function to get elements with fetch added

--- chamber/js/json.js
+++ chamber/js/json.js
@@ -1,4 +1,5 @@
 let newElement= document.querySelector('#asd')
+let business= document.querySelector('#business')
 
 const doStuf = function(data){
     console.log(data[0].image);
@@ -6,8 +7,31 @@ const doStuf = function(data){
     let img= document.createElement('img')
     img.setAttribute('src', image);
     newElement.appendChild(img)
-
 }
+const displayBusiness= function(business){
+    let image=business.image;
+    let name=business.name;
+    let adress=business.adress;
+    let phone=business.phone;
+    let website=business.website;
+    let card = document.createElement('section');
+    let img=document.createElement('img');
+    img.setAttribute('src', image);
+    img.setAttribute('alt', name);
+    let p1=document.createElement('p');
+    let p2=document.createElement('p');
+    let p3=document.createElement('p')
+    p1.innerText=adress;
+    p2.innerText=phone;
+    p3.innerText=website;
+    card.appendChild(img);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(p3);
+    business.appendChild(card);
+}
+
+
 fetch('https://cocotero98.github.io/wdd230/chamber/data/data.json')
 .then((response)=>{return response.json()})
-.then(doStuf)
+.then(displayBusiness)
