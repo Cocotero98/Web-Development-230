1b7a2cfc6cc26c0f01c722d10b7feba6b974530f
Cocotero98 <aguilarag98@gmail.com>
2022-03-09 23:49:13 -0300

    .

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,10 +1,10 @@
+let apiURL='https://api.openweathermap.org/data/2.5/weather?id=3841490&appid=0cd66e608ce7503372a31896aad04b0e'
+
 let temperatureInt=''
 let wSpeedInt=''
-let temperature=document.getElementById('temperature').innerHTML
-let windSpeed=document.getElementById('windSpeed').innerHTML
+// let temperature=document.getElementById('temperature').innerHTML
+// let windSpeed=document.getElementById('windSpeed').innerHTML
 
-wSpeedInt=parseFloat(windSpeed)
-temperatureInt=parseInt(temperature,10)
 
 let f=function(c){
     return (c*1.8)+32
@@ -15,7 +15,9 @@ let m=function (k){
     return  k*0.6214
 }
 let miles= m(wSpeedInt)
-
+let k=function (m){
+    return m/0.6214
+}
 let c=function(f){
     return (f-32)/1.8
 }
@@ -27,6 +29,33 @@ let windChillCalculator= function(t,s){
         return windChill
     }else{return 'N/A'}
 }
-let resultInFarenheit=windChillCalculator(farenheit,miles)
-let windChillInC=c(resultInFarenheit)
-document.getElementById('windChill').innerHTML=`Wind Chill: ${windChillInC.toFixed(1)}°C`
+
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let farenheitTemperature=jsObject.main.temp
+    let windSpeed=jsObject.wind.speed
+    let windChill=windChillCalculator(farenheitTemperature,windSpeed)
+    let windChillInC=c(windChill)
+    let celsiusTemperature=c(farenheitTemperature)
+    let wSpeedKm=k(windSpeed)
+    document.querySelector('#temperature').textContent = `${celsiusTemperature.toFixed(0)}°C`;
+    document.querySelector('#windSpeed').textContent=`${wSpeedKm.toFixed(1)}km/h`
+    document.querySelector('#windChill').textContent=`${windChillInC}°C`
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    // document.querySelector('#weatherIcon').textContent = iconsrc;
+    document.querySelector('#weatherIcon').setAttribute('src', iconsrc);
+    document.querySelector('#weatherIcon').setAttribute('alt', desc);
+    // document.querySelector('figcaption').textContent = desc;
+  });
+// wSpeedInt=parseFloat(windSpeed)
+// temperatureInt=parseInt(temperature,10)
+
+
+// let resultInFarenheit=windChillCalculator(farenheit,miles)
+// let windChillInC=c(resultInFarenheit)
+// document.getElementById('windChill').innerHTML=`Wind Chill: ${windChillInC.toFixed(1)}°C`
--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,8 +1,14 @@
-let apiURL='https://api.openweathermap.org/data/2.5/forecast?id=5861897&appid=0cd66e608ce7503372a31896aad04b0e'
-console.log('asd')
+let apiURL='https://api.openweathermap.org/data/2.5/weather?id=5861897&appid=0cd66e608ce7503372a31896aad04b0e'
+
 fetch(apiURL)
-  .then((response) => console.log('yes'),response.json())
+  .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    console.log('yes')
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
   });
