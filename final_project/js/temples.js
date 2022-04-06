446f4a039680c505d5859096b853f15afb50f830
Cocotero98 <aguilarag98@gmail.com>
2022-04-06 10:41:22 -0300

    .

--- final_project/js/temples.js
+++ final_project/js/temples.js
@@ -1,3 +1,3 @@
-fetch('../data/data.json')
+fetch('https://cocotero98.github.io/wdd230/final_project/data/data.json')
 .then(response=>response.json())
 .then(jsonData=>console.log(jsonData.temples))
