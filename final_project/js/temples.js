db138b17306b10fb2e6e51a4fac5c0e7a64c394d
Cocotero98 <aguilarag98@gmail.com>
2022-04-06 16:31:30 -0300

    finished temples?

--- final_project/js/temples.js
+++ final_project/js/temples.js
@@ -1,4 +1,8 @@
 let templeSection=document.querySelector('#temple-cards');
+let firstSection=document.querySelector('#first_part');
+let secondSection=document.querySelector('#second_part');
+
+
 
 fetch('https://cocotero98.github.io/wdd230/final_project/data/data.json')
 .then(response=>response.json())
@@ -10,17 +14,20 @@ fetch('https://cocotero98.github.io/wdd230/final_project/data/data.json')
 const createCard=function(temple){
     let card=document.createElement('div');
     let firstPart=document.createElement('div');
+    firstPart.setAttribute('id','first_part');
     let secondPart=document.createElement('div');
+    secondPart.setAttribute('id','second_part');
     let img=document.createElement('img');
     let name=document.createElement('h3');
     let ordinanceInfo=document.createElement('p');
     let adress=document.createElement('p');
     let tel=document.createElement('p');
-    let history=document.createElement('p');
     let services=document.createElement('p');
-    let closure=document.createElement('p');
     let btnOne=document.createElement('button');
     let btnTwo=document.createElement('button');
+    let buttons=document.createElement('div');
+    buttons.appendChild(btnOne);
+    buttons.appendChild(btnTwo);
     img.setAttribute('src',temple.image);
     img.setAttribute('alt',temple.name);
     name.innerText=temple.name;
@@ -28,23 +35,48 @@ const createCard=function(temple){
     adress.innerText=temple.adress
     tel.innerText=temple.telephone;
     let historyLi=temple.history.map((line)=>`<li>${line}</li>`);
-    history.innerHTML=`<p>Important dates:</p><ul>${historyLi.join('')}</ul>`;
     services.innerText=temple.services;
     let closuresLi=temple.closure.map((line)=>`<li>${line}</li>`);
-    closure.innerHTML=`<p>Closures:</p><ul>${closuresLi.join('')}</ul>`;
     btnOne.innerText=1;
     btnTwo.innerText=2;
+    btnOne.setAttribute('id','firstBtn');
+    btnTwo.setAttribute('class','secondBtn')
     card.appendChild(img);
     card.appendChild(name);
     firstPart.appendChild(ordinanceInfo);
     firstPart.appendChild(services);
     firstPart.appendChild(adress);
     firstPart.appendChild(tel);
-    secondPart.appendChild(history);
-    secondPart.appendChild(closure);
+    secondPart.innerHTML=`<p>Important dates:</p><ul>${historyLi.join('')}</ul><p>Closures:</p><ul>${closuresLi.join('')}</ul>`;
     card.appendChild(firstPart);
     card.appendChild(secondPart);
-    card.appendChild(btnOne);
-    card.appendChild(btnTwo);
+    btnOne.addEventListener('click',()=>{
+        firstPart.style.display='block';
+        secondPart.style.display='none';
+        btnOne.style.backgroundColor='#721121';
+        btnOne.style.color='white';
+        btnTwo.style.backgroundColor='white';
+        btnTwo.style.color='#721121';
+    });
+    btnTwo.addEventListener('click',()=>{
+        firstPart.style.display='none';
+        secondPart.style.display='block';  
+        btnTwo.style.backgroundColor='#721121';
+        btnTwo.style.color='white';  
+        btnOne.style.backgroundColor='white';
+        btnOne.style.color='#721121';              
+    })
+    card.appendChild(buttons);
     templeSection.appendChild(card);
 }
+
+
+//----------------------------Toggle menu for info displayed-------------------------------------
+
+// let firstBtn=document.querySelector('button')
+// const secondBtn=document.querySelector('.secondBtn');
+// let firstSection=document.querySelector('#first_part');
+// let secondSection=document.querySelector('#second_part');
+// console.log(secondBtn)
+// firstBtn.addEventListener('click',showFirstSection);
+// secondBtn.addEventListener('click',showSecondSection);
