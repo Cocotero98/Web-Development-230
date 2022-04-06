020f74dd760f2937f50985a81414f6ffe3b0d2f2
Cocotero98 <aguilarag98@gmail.com>
2022-04-06 14:19:42 -0300

    Temple json and js finished

--- final_project/js/temples.js
+++ final_project/js/temples.js
@@ -1,3 +1,50 @@
+let templeSection=document.querySelector('#temple-cards');
+
 fetch('https://cocotero98.github.io/wdd230/final_project/data/data.json')
 .then(response=>response.json())
-.then(jsonData=>console.log(jsonData.temples))
+.then(jsonData=>{
+    const temples=jsonData.temples;
+    temples.forEach(createCard);
+})
+
+const createCard=function(temple){
+    let card=document.createElement('div');
+    let firstPart=document.createElement('div');
+    let secondPart=document.createElement('div');
+    let img=document.createElement('img');
+    let name=document.createElement('h3');
+    let ordinanceInfo=document.createElement('p');
+    let adress=document.createElement('p');
+    let tel=document.createElement('p');
+    let history=document.createElement('p');
+    let services=document.createElement('p');
+    let closure=document.createElement('p');
+    let btnOne=document.createElement('button');
+    let btnTwo=document.createElement('button');
+    img.setAttribute('src',temple.image);
+    img.setAttribute('alt',temple.name);
+    name.innerText=temple.name;
+    ordinanceInfo.innerText=temple.ordinance_schedule;
+    adress.innerText=temple.adress
+    tel.innerText=temple.telephone;
+    let historyLi=temple.history.map((line)=>`<li>${line}</li>`);
+    history.innerHTML=`<p>Important dates:</p><ul>${historyLi.join('')}</ul>`;
+    services.innerText=temple.services;
+    let closuresLi=temple.closure.map((line)=>`<li>${line}</li>`);
+    closure.innerHTML=`<p>Closures:</p><ul>${closuresLi.join('')}</ul>`;
+    btnOne.innerText=1;
+    btnTwo.innerText=2;
+    card.appendChild(img);
+    card.appendChild(name);
+    firstPart.appendChild(ordinanceInfo);
+    firstPart.appendChild(services);
+    firstPart.appendChild(adress);
+    firstPart.appendChild(tel);
+    secondPart.appendChild(history);
+    secondPart.appendChild(closure);
+    card.appendChild(firstPart);
+    card.appendChild(secondPart);
+    card.appendChild(btnOne);
+    card.appendChild(btnTwo);
+    templeSection.appendChild(card);
+}
